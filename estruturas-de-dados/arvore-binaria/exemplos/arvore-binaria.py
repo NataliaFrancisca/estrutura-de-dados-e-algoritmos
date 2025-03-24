95655befc6d31185c679ce427f614db6c5dafe1d
nataliafrancisca <nathifrancisca@gmail.com>
2025-03-23 21:34:14 -0300

    feat: exibir árvore binária por níveis
    Método retorna um array onde o primeiro elemento é a root e os seguintes representam os nós organizados por nível.

--- estruturas-de-dados/arvore-binaria/exemplos/arvore-binaria.py
+++ estruturas-de-dados/arvore-binaria/exemplos/arvore-binaria.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Folha:
     def __init__(self, valor, esquerda, direita):
         self.valor = valor
@@ -8,6 +10,32 @@ class Folha:
 class ArvoreBinaria:
     def __init__(self):
         self.head = None
+
+    def exibir(self):
+        if self.head is None:
+            return []
+
+        fila = deque([self.head])
+        resultado = []
+
+        while fila:
+            tamanho_de_nos_por_nivel = len(fila)
+            nos_do_nivel = []
+
+            for _ in range(tamanho_de_nos_por_nivel):
+                nodo = fila.popleft()
+                nos_do_nivel.append(nodo.valor)
+
+                if nodo.esquerda:
+
+                    fila.append(nodo.esquerda)
+                if nodo.direita:
+  
+                    fila.append(nodo.direita)
+
+            resultado.append(nos_do_nivel)
+
+        print(resultado)
 
     def inserir(self, valor):
         if(self.head is None):
@@ -41,36 +69,25 @@ arvorebinaria.inserir(10)
 arvorebinaria.inserir(8)
 arvorebinaria.inserir(15)
 
-print("ROOT: ", arvorebinaria.head.valor)
-print("1. ESQUERDA: ", arvorebinaria.head.esquerda.valor)
-print("1. DIREITA: ", arvorebinaria.head.direita.valor)
 
 arvorebinaria.inserir(14)
 arvorebinaria.inserir(17)
-print("2. ESQUERDA: ", arvorebinaria.head.direita.esquerda.valor)
-print("2. DIREITA: ", arvorebinaria.head.direita.direita.valor)
 
 arvorebinaria.inserir(20)
 arvorebinaria.inserir(2)
-print("3. ESQUERDA: ", arvorebinaria.head.esquerda.esquerda.valor)
-print("3. DIREITA: ", arvorebinaria.head.direita.direita.direita.valor)
 
 
 arvorebinaria.inserir(1)
 arvorebinaria.inserir(3)
-print("4. ESQUERDA: ", arvorebinaria.head.esquerda.esquerda.esquerda.valor)
-print("4. DIREITA: ", arvorebinaria.head.esquerda.esquerda.direita.valor)
+
 
 arvorebinaria.inserir(4)
 arvorebinaria.inserir(6)
-print("5. DIREITA: ", arvorebinaria.head.esquerda.esquerda.direita.direita.valor)
-print("6. DIREITA: ", arvorebinaria.head.esquerda.esquerda.direita.direita.direita.valor)
 
 arvorebinaria.inserir(9)
 arvorebinaria.inserir(0)
-print("2. ESQUERDA: ", arvorebinaria.head.esquerda.direita.valor)
-print("5. ESQUERDA: ", arvorebinaria.head.esquerda.esquerda.esquerda.esquerda.valor)
 
 arvorebinaria.inserir(0)
 arvorebinaria.inserir(0)
-print("Contador: ", arvorebinaria.head.esquerda.esquerda.esquerda.esquerda.contador)
+
+arvorebinaria.exibir()
