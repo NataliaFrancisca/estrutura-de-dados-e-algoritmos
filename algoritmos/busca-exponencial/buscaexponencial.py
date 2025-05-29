1a326238162b2ddf94a40d5c882e78eddcf1c4b6
nataliafrancisca <nathifrancisca@gmail.com>
2025-05-28 22:46:20 -0300

    feat: fazendo melhorias na resolução

--- algoritmos/busca-exponencial/buscaexponencial.py
+++ algoritmos/busca-exponencial/buscaexponencial.py
@@ -1,7 +1,7 @@
-lista = [2,3,4,5,6,7,8,9,14,15,16,19,21];
+lista = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,19,21];
 listaB = [1,2,3,4,5,6]
 
-def buscaBinaria(lista, valor, pesquerda, pdireita):
+def buscaBinaria(lista, valor, pesquerda=0, pdireita=None):
     while pesquerda <= pdireita:
         meio = (pdireita + pesquerda) // 2;
 
@@ -20,17 +20,22 @@ def buscaExponencial(lista, valor):
     if lista[0] == valor:
         return 0
 
+    pesquerda = 0
     pdireita = 1
 
-    while pdireita < len(lista):
-       pdireita *= 2
+    while pdireita < len(lista) and lista[pdireita] < valor:
+        pesquerda = pdireita
+        pdireita *= 2
+    
+    if lista[pdireita // 2] == valor:
+        return pdireita // 2
 
-    return buscaBinaria(lista, valor, 0, pdireita)
+    return buscaBinaria(lista, valor, pesquerda, min(pdireita, len(lista) - 1))
 
 a = buscaExponencial(lista, 14)
-b = buscaExponencial(lista, 10)
+b = buscaExponencial(lista, 100)
 c = buscaExponencial(listaB, 2)
 
-print(a)
-print(b)
-print(c)
+print("resultado: ", a)
+print("resultado: ", b)
+print("resultado: ", c)
